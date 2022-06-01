f5ca1d0689064a73d577ef305b525e8ca5b404c0
hybridpicker <lukas@schoensgibl.com>
2022-06-01 06:21:47 +0200

    base setup css

--- musica_sacra/urls.py
+++ musica_sacra/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 import home.views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home.views.home, name='home_view'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
