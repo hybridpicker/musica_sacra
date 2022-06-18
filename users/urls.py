677f91205a95a89df7371251283b048cd0c0fd6d
lschoensgibl@gmail.com <schoensgibl@MacBook-Air-von-Lukas.local>
2022-06-18 14:08:47 +0200

    settings review

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import HomePageView
 
 urlpatterns = [
-#    path('', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('', HomePageView.as_view(), name='user_home'),
     path('password/change', views.change_password, name='change_password'),
